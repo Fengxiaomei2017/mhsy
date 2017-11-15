ed092962253c2564d4e262e9194ffc9cc657f961
fengxiaomei <fengxiaomei@mhsy.com>
2017-11-15 16:26:38 +0800

    把字体单位由rem改为em

--- src/components/dtbangdingshebei/Dtbangdingshebei.vue
+++ src/components/dtbangdingshebei/Dtbangdingshebei.vue
@@ -20,15 +20,15 @@
     position: relative;
     width: 100vw;
     height: 100vh;
-    font-size: 1rem;
+    font-size: 1em;
     text-align: center;
     background-color: #eee;
   }
   .bianhao .shujubox {
     position: absolute;
     width: 95%;
-    height: 250px;
-    padding-top: 40px;
+    height: 40vh;
+    padding-top: 5vh;
     top: 20%;
     left: 50%;
     transform: translate(-50%, 0);
@@ -38,29 +38,30 @@
     background-color: #fff;
   }
   .bianhao .shujubox h2 {
-    font-size: 1.3rem;
-    margin: 0 auto 20px auto;
+    margin: 0 auto 3vh auto;
+  }
+  .bianhao .shuru label,.bianhao .shuru input {
+    width: 80vw;
+    margin: 2vh 6vw;
+    font-size: 1.2em;
+  }
+  .bianhao .shuru input,.bianhao button {
+    height: 7vh;
+    line-height: 7vh;
   }
   .bianhao .shuru label {
     display: block;
-    width: 90%;
-    margin: 30px 20px;
     text-align: left;
   }
   .bianhao .shuru input {
-    width: 80vw;
-    height: 40px;
     margin-bottom: 30px;
-    font-size: 1rem;
-    line-height: 40px;
     border: 0;
     border-bottom: 1px solid #A1A0A0;
   }
   .bianhao button {
-    width: 60%;
-    height: 40px;
-    font-size: 1.1rem;
-    line-height: 40px;
+    width: 60vw;
+    max-width: 200px;
+    font-size: 1.2em;
     border: 1px solid #5FADFF;
     border-radius: 8px;
     background-color: #03A9F4;
